4e9de1df8926c3a426e132022a65e52134d0475a
duckfoo123 <duckfoo1322@gmail.com>
2024-11-30 12:56:11 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "oncommandkotlin"
+
+val prefix = rootProject.name
+
+include("$prefix-api", "$prefix-core", "$prefix-plugin")
+
+val dongle = "$prefix-dongle"
+val dongleFile = file(dongle)
+if (dongleFile.exists()) {
+    include(dongle)
+//     load nms
+    dongleFile.listFiles()?.filter {
+        it.isDirectory && it.name.startsWith("v")
+    }?.forEach { file ->
+        include(":$dongle:${file.name}")
+    }
+}
+
+val publish = "$prefix-publish"
+if (file(publish).exists()) include(publish)
---
+++ src/main/kotlin/io/github/duckfooux/commandkotlin/plugin.kt
@@ -0,0 +1,14 @@
+package io.github.duckfooux.commandkotlin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class plugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
